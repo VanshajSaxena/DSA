6e7ede9f8c36dd70d12f1094a03cffe0dfbb2944
Vanshaj Saxena <vs110405@outlook.com>
2024-08-01 00:23:31 +0530

    rewrite: LinkedList

--- src/LinkedLists/LinkedList.java
+++ src/LinkedLists/LinkedList.java
@@ -2,36 +2,34 @@
  * LinkedList
  */
 public class LinkedList {
-  static class Node {
-    int data;
+  Node head;
+
+  class Node {
+    String data;
     Node next;
 
-    Node(int data) {
+    Node(String data) {
       this.data = data;
       this.next = null;
     }
 
   }
 
-  private Node head = null;
-
-  LinkedList(Node head) {
+  LinkedList(String data) {
+    Node head = new Node(data);
     this.head = head;
   }
 
-  LinkedList() {
-  }
-
-  void printLinkedList() {
-    Node current = head;
-    while (current != null) {
-      System.out.println(current.data);
-      current = current.next;
+  void printList() {
+    Node n = this.head;
+    while (n != null) {
+      System.out.println(n.data + " ");
+      n = n.next;
     }
   }
 
   public static void main(String[] args) {
-    LinkedList myList = new LinkedList();
-    myList.printLinkedList();
+    LinkedList myList = new LinkedList("A");
+    myList.printList();
   }
 }
