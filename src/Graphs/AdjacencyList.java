5c74dd0386264e4fdcf1eed07ec3c35429d4717f
Vanshaj Saxena <vs110405@outlook.com>
2024-09-08 04:21:41 +0530

    Graphs: AdjacencyList, AdjacencyMatrix

---
+++ src/Graphs/AdjacencyList.java
@@ -0,0 +1,78 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/**
+ * AdjacencyList
+ */
+public class AdjacencyList {
+
+  public static void main(String[] args) {
+    Graph graph = new Graph();
+
+    graph.addNode(new Node("A"));
+    graph.addNode(new Node("B"));
+    graph.addNode(new Node("C"));
+    graph.addNode(new Node("D"));
+    graph.addNode(new Node("E"));
+
+    graph.addEdge(0, 1);
+    graph.addEdge(1, 2);
+    graph.addEdge(1, 3);
+    graph.addEdge(2, 4);
+    graph.addEdge(2, 3);
+    graph.addEdge(4, 0);
+    graph.addEdge(4, 0);
+
+    graph.print();
+  }
+}
+
+class Graph {
+  ArrayList<LinkedList<Node>> alist;
+
+  Graph() {
+    this.alist = new ArrayList<>();
+  }
+
+  public void addNode(Node node) {
+    LinkedList<Node> currentList = new LinkedList<>();
+
+    currentList.add(node);
+
+    alist.add(currentList);
+  }
+
+  public void addEdge(int src, int dst) {
+    LinkedList<Node> currentList = alist.get(src);
+    Node dstNode = alist.get(dst).get(0);
+    currentList.add(dstNode);
+  }
+
+  public boolean checkNode(int src, int dst) {
+    LinkedList<Node> currentList = alist.get(src);
+    Node dstNode = alist.get(dst).get(0);
+    for (Node node : currentList) {
+      if (node == dstNode) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public void print() {
+    for (LinkedList<Node> currentList : alist) {
+      for (Node node : currentList) {
+        System.out.print(node.data + " -> ");
+      }
+      System.out.println();
+    }
+  }
+}
+
+class Node {
+  String data;
+
+  Node(String data) {
+    this.data = data;
+  }
+}
---
+++ src/Graphs/AdjacencyMatrix.java
@@ -0,0 +1,78 @@
+import java.util.ArrayList;
+
+/**
+ * AdjacencyMatrix
+ */
+public class AdjacencyMatrix {
+
+  public static void main(String[] args) {
+    Graph graph = new Graph(5);
+    graph.addNode(new Node("A"));
+    graph.addNode(new Node("B"));
+    graph.addNode(new Node("C"));
+    graph.addNode(new Node("D"));
+    graph.addNode(new Node("E"));
+
+    graph.addEdge(0, 1);
+    graph.addEdge(0, 2);
+    graph.addEdge(1, 3);
+    graph.addEdge(2, 3);
+    graph.addEdge(3, 4);
+    graph.addEdge(4, 4);
+    graph.addEdge(4, 0);
+
+    graph.print();
+    System.out.println(graph.checkEdge(0, 0));
+    ;
+  }
+}
+
+class Graph {
+
+  ArrayList<Node> nodes;
+  int[][] matrix;
+
+  Graph(int size) {
+    nodes = new ArrayList<>();
+    matrix = new int[size][size];
+  }
+
+  public void addNode(Node node) {
+    nodes.add(node);
+  }
+
+  public void addEdge(int src, int dst) {
+    matrix[src][dst] = 1;
+  }
+
+  public boolean checkEdge(int src, int dst) {
+    if (matrix[src][dst] == 1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public void print() {
+    System.out.print("  ");
+    for (Node node : nodes) {
+      System.out.print(node.data + " ");
+    }
+    System.out.println();
+    for (int i = 0; i < matrix.length; i++) {
+      System.out.print(nodes.get(i).data + " ");
+      for (int j = 0; j < matrix[i].length; j++) {
+        System.out.print(matrix[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
+}
+
+class Node {
+  String data;
+
+  Node(String data) {
+    this.data = data;
+  }
+}
